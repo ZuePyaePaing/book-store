b74ab9a7a91cfb20011bc4a1a7638892391929b5
ZuePyaePaing <kaunglay24588@gmail.com>
2024-02-01 21:36:35 -0800

    first cmmit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "http://localhost:8000/";
+
+export const navLink = [
+  { label: "Home", href: "/" },
+  { label: "Home", href: "/" },
+  { label: "Home", href: "/" },
+  { label: "Home", href: "/" },
+];
---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useFetchData = (endpoint) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}${endpoint}`);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+      return { data, loading, error };
+    };
+
+    fetchData();
+  }, [endpoint]);
+};
+
+export default useFetchData
